b89c585d0dbf9106529d589fe0732fb595734518
abhayhk29 <abhayhk29@gmail.com>
2024-07-27 12:46:09 +0530

    initial phase

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import {sum} from "@autospace/sample-lib"
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + sum(34,454);
   }
 }
