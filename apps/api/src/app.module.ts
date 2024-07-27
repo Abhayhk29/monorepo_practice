55d3b7c00ba0e22130e8a2b8be2b5106127130b7
abhayhk29 <abhayhk29@gmail.com>
2024-07-27 16:04:55 +0530

    basic code practice

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path';
+import {ConfigModule} from "@nestjs/config"
+import { PrismaModule } from './common/prisma/prisma.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        buildSchemaOptions: {
+           numberScalarMode: 'integer',
+        },
+    }),
+
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
